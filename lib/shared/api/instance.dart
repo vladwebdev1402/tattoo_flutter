5c23c86ad87db9d38de457f81f4735a7faa04666
vladwebdev1402 <mmovlad.kriven.1402@gmail.com>
2024-11-22 01:49:18 +0700

    feat: add assets

--- lib/shared/api/instance.dart
+++ lib/shared/api/instance.dart
@@ -4,7 +4,7 @@ class ApiInstance {
   static const String baseUrl = "http://192.168.0.18:3050/api";
 
   static Future<http.Response> get(String url,
-      [Map<String, dynamic>? params = null]) async {
+      [Map<String, dynamic>? params]) async {
     final uri = Uri.parse(baseUrl + url);
     return await http.get(uri);
   }
